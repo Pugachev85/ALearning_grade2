e7345532c16acdd0e866aecb78606019ff03e814
Pugachev85 <workpost2009@mail.ru>
2025-11-15 22:47:54 +0300

    Apply reviewer's feedback

--- src/main/java/com/ALearning_grade2/dto/UserDTO.java
+++ src/main/java/com/ALearning_grade2/dto/UserDTO.java
@@ -6,8 +6,9 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -16,7 +17,8 @@ import java.time.LocalDateTime;
  * Используется для передачи данных между слоями приложения
  * и валидации входящих данных.
  */
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
